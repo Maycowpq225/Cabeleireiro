3d460e64b409edcd0cc644e976bcd904c215e505
Maycowpq225 <maycowfellipo@hotmail.com>
2020-10-07 08:07:30 -0300

    Layout de login e registro efetuados com sucesso!

---
+++ app/src/main/java/com/example/CabeleireiroAgendamento1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.CabeleireiroAgendamento1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registrarconta);
+    }
+
+    public void clicar(View e){
+
+    }
+}
