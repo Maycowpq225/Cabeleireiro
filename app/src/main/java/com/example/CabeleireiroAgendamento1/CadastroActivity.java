e8b0ad854c1ba01e93b1a4fe25b02aefae0a3b3f
Maycowpq225 <maycowfellipo@hotmail.com>
2020-10-08 22:08:47 -0300

    Fazendo conexão com banco de dados

--- app/src/main/java/com/example/CabeleireiroAgendamento1/CadastroActivity.java
+++ app/src/main/java/com/example/CabeleireiroAgendamento1/CadastroActivity.java
@@ -3,17 +3,12 @@ package com.example.CabeleireiroAgendamento1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class CadastroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void clicar(View e){
-
+        setContentView(R.layout.activity_cadastro);
     }
 }
---
+++ app/src/main/java/com/example/CabeleireiroAgendamento1/funcoes/Cliente.java
@@ -0,0 +1,13 @@
+package com.example.CabeleireiroAgendamento1.funcoes;
+
+public class Cliente extends Default {
+
+    private String name;
+    private String email;
+    private String senha;
+
+    public Cliente() {
+        super();
+    }
+
+}
